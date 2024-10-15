fe016d4a99d0fb7a719c7d7ca1ba238b1019276a
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-10-15 10:15:57 -0600

    Se crea migración para cambio de tipo de dato en columnas para id de usuario de entero a string.

---
+++ ERPSEI/Data/Migrations/20241015161512_UpdateConciliacionesJorge.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConciliacionesJorge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UsuarioModificadorId",
+                table: "Conciliaciones",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UsuarioCreadorId",
+                table: "Conciliaciones",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioModificadorId",
+                table: "Conciliaciones",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UsuarioCreadorId",
+                table: "Conciliaciones",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
