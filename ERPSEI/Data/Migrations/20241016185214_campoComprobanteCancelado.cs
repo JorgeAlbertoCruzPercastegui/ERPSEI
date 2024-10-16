20810a22fbec85305f6f15afecb37058b3c6a068
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-10-16 12:52:50 -0600

    Se agrega y se aplica migración para incluir campo "Cancelado" a los comprobantes

---
+++ ERPSEI/Data/Migrations/20241016185214_campoComprobanteCancelado.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class campoComprobanteCancelado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Cancelado",
+                table: "Comprobantes",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cancelado",
+                table: "Comprobantes");
+        }
+    }
+}
