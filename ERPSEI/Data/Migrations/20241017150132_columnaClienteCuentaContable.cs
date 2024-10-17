eef98a21a6bd16bfd70e38e8777a8fab9682cdcc
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-10-17 09:03:46 -0600

    Se agrega y aplica migración para incluir columna cuenta contable en entidad cliente

---
+++ ERPSEI/Data/Migrations/20241017150132_columnaClienteCuentaContable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class columnaClienteCuentaContable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CuentaContable",
+                table: "Clientes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CuentaContable",
+                table: "Clientes");
+        }
+    }
+}
