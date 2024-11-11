dddca5a680a0c08dba3ba6e5e4080d1ea61b9588
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-11-11 11:41:15 -0600

    Se modifica el permiso para validar comprobantes para que solo se pueda realizar cuando se tienen permisos de consulta.

--- ERPSEI/Areas/ERP/Pages/AdministradorDeComprobantes.cshtml.cs
+++ ERPSEI/Areas/ERP/Pages/AdministradorDeComprobantes.cshtml.cs
@@ -828,7 +828,7 @@ namespace ERPSEI.Areas.ERP.Pages
 		{
 			ServerResponse resp = new(true, localizer["ComprobantesValidatedUnsuccessfully"]);
 			List<Comprobante> comprobantes = [];
-			if (PuedeTodo || PuedeEditar)
+			if (PuedeTodo || PuedeConsultar)
 			{
 				try
 				{
