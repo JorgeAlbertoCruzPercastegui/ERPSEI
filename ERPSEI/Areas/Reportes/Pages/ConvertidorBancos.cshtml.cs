e45bc29ccebbbe18d235b02cd95b6a5c9ce97811
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2024-11-22 17:42:55 -0600

    Se creó el proyecto ConvertidorBancos

---
+++ ERPSEI/Areas/Reportes/Pages/ConvertidorBancos.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ERPSEI.Areas.Reportes
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConvertidorBancos : ControllerBase
+    {
+        // GET: api/<ConvertidorBancos>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ConvertidorBancos>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ConvertidorBancos>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ConvertidorBancos>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ConvertidorBancos>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
