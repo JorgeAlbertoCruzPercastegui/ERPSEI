4d69c8721c128a3e07c49145f661b5583e8de27a
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2024-11-22 18:33:38 -0600

    Se modificó estructura de los archivos del convertidor

---
+++ ERPSEI/Areas/Reportes/Pages/ConvertidorBancario.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERPSEI.Areas.Reportes.Pages
+{
+    public class ConvertidorBancario : PageModel
+    {
+    }
+}
