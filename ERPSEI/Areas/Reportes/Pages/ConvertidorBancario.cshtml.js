1f4d790f22d28998d19d90cbe915f5bfc2c9bd20
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2024-11-25 11:38:34 -0600

    Se agregó la vista del convertidor con la conexion de los bancos a seleccionar

--- ERPSEI/Areas/Reportes/Pages/ConvertidorBancario.cshtml.js
+++ ERPSEI/Areas/Reportes/Pages/ConvertidorBancario.cshtml.js
@@ -1,1 +1,78 @@
-﻿
+﻿var table;
+
+const NUEVO = 0;
+const EDITAR = 1;
+const VER = 2;
+const maxFileSizeInBytes = 5242880; //5mb = (5 * 1024) * 1024;
+const oneMegabyteSizeInBytes = 1048576; // 1mb = (1 * 1024) * 1024
+const postOptions = {
+    headers: {
+        "RequestVerificationToken": $('input[name="__RequestVerificationToken"]').val()
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    table = $("#table");
+    initTable();
+});
+
+function initTable() {
+    table.bootstrapTable('destroy').bootstrapTable({
+        height: 550,
+        locale: cultureName,
+        exportDataType: 'all',
+        exportTypes: ['excel'],
+        toolbar: '#toolbar', // Asegúrate de que este ID coincida con el elemento HTML donde quieres que aparezcan los botones
+        columns: [
+            {
+                title: colFechaMovimientoHeader,
+                field: "FechaMovimiento",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colFechaAplicacionHeader,
+                field: "FechaAplicacion",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colReferenciaHeader,
+                field: "Referencia",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colDescripcionHeader,
+                field: "Descripcion",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colCargoHeader,
+                field: "Cargo",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colAbonoHeader,
+                field: "Abono",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colSaldoHeader,
+                field: "Saldo",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            }
+        ]
+    });
+}
