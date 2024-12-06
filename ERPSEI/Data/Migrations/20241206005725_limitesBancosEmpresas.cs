94c61276e626373dbe1060204d539082406ed80f
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-12-05 19:00:13 -0600

    Se crea y aplica migración para el campo límite de bancos de empresas.

---
+++ ERPSEI/Data/Migrations/20241206005725_limitesBancosEmpresas.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPSEI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class limitesBancosEmpresas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Limite",
+                table: "BancosEmpresa",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Limite",
+                table: "BancosEmpresa");
+        }
+    }
+}
