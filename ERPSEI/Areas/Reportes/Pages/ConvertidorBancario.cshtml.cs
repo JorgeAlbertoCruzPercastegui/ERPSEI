9e49fc0b7d24fbb517ebdd1d063523dc2670570a
Luis Linares <74635129+OmeOllin@users.noreply.github.com>
2024-12-13 17:25:30 -0600

    Se agrega la política de autorización estandar al convertidor bancario para dar acceso desde el módulo de roles.

--- ERPSEI/Areas/Reportes/Pages/ConvertidorBancario.cshtml.cs
+++ ERPSEI/Areas/Reportes/Pages/ConvertidorBancario.cshtml.cs
@@ -1,26 +1,13 @@
-using ERPSEI.Data.Entities.Empleados;
-using ERPSEI.Data.Entities.Reportes;
-using ERPSEI.Data.Managers.Empleados;
-using ERPSEI.Data.Managers.Reportes;
+using ERPSEI.Data.Entities.Conciliaciones;
 using ERPSEI.Pages.Shared;
-using ERPSEI.Requests;
-using ExcelDataReader;
+using ERPSEI.Resources;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Localization;
-using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
-using System.Data;
-using System.Net.Mime;
-using System.Globalization;
-using ERPSEI.Data.Entities.Conciliaciones;
-using static ERPSEI.Areas.ERP.Pages.ConciliacionesModel;
-using ERPSEI.Data.Entities.SAT.cfdiv40;
-using ERPSEI.Resources;
 
 namespace ERPSEI.Areas.Reportes.Pages
 {
-	[Authorize]
+	[Authorize(Policy = "AccessPolicy")]
 	public class ConvertidorBancarioModel : ERPPageModel
 	{
 		[BindProperty]
