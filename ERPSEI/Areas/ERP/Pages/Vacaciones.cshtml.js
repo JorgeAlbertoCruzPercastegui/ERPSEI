01305c2ec4bfd77fb4c4382e984fb15ef24cf972
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2025-05-30 14:05:13 -0600

    Diseño básico Vacaciones

--- ERPSEI/Areas/ERP/Pages/Vacaciones.cshtml.js
+++ ERPSEI/Areas/ERP/Pages/Vacaciones.cshtml.js
@@ -1,1 +1,209 @@
-﻿
+﻿var table;
+var buttonRemove;
+var selections = [];
+var dlg = null;
+var dlgModal = null;
+
+const NUEVO = 0;
+const EDITAR = 1;
+const VER = 2;
+const postOptions = { headers: { "RequestVerificationToken": $('input[name="__RequestVerificationToken"]').val() } }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    table = $("#table");
+    buttonRemove = $("#remove");
+    //buttonExportAll = $("#exportAll");
+    dlg = document.getElementById('dlgVacaciones');
+
+    if (dlg) {
+        dlgModal = new bootstrap.Modal(dlg, null);
+
+        dlg.addEventListener('hidden.bs.modal', function (event) {
+            onCerrarClick();
+        });
+    } else {
+        console.error("No se encontró el modal con id #dlgVacaciones");
+    }
+
+    initTable();
+});
+
+function getIdSelections() {
+    return $.map(table.bootstrapTable('getSelections'), function (row) {
+        return row.id
+    })
+}
+function responseHandler(res) {
+    $.each(res, function (i, row) {
+        row.state = $.inArray(row.id, selections) !== -1
+    })
+    return res
+}
+
+function operateFormatter(value, row, index) {
+    let icons = [];
+
+    //Icono Ver
+    icons.push(`<li><a class="dropdown-item see" href="#" title="${btnVerTitle}"><i class="bi bi-search"></i> ${btnVerTitle}</a></li>`);
+    //Icono Editar
+    icons.push(`<li><a class="dropdown-item edit" href="#" title="${btnEditarTitle}"><i class="bi bi-pencil-fill"></i> ${btnEditarTitle}</a></li>`);
+
+    return `<div class="dropdown">
+              <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-three-dots-vertical success"></i>
+              </button>
+              <ul class="dropdown-menu">${icons.join("")}</ul>
+            </div>`;
+}
+window.operateEvents = {
+    'click .see': function (e, value, row, index) {
+        initActivoFijoDialog(VER, row);
+    },
+    'click .edit': function (e, value, row, index) {
+        initActivoFijoDialog(EDITAR, row);
+        //table.bootstrapTable('remove', {
+        //    field: 'id',
+        //    values: [row.id]
+        //})
+    }
+}
+
+function onAgregarClick() {
+    initActivoFijoDialog(NUEVO, { id: "Nuevo", nombre: "" });
+}
+function initTable() {
+    table.bootstrapTable('destroy').bootstrapTable({
+        height: 550,
+        locale: cultureName,
+        exportDataType: 'all',
+        exportTypes: ['excel'],
+        columns: [
+            {
+                field: "state",
+                checkbox: true,
+                align: "center",
+                valign: "middle"
+            },
+            {
+                title: "Id",
+                field: "id",
+                align: "center",
+                valign: "middle",
+                sortable: true,
+                width: "80px"
+            },
+            {
+                title: "Empleado",
+                field: "empleado",
+                align: "center",
+                valign: "middle",
+                sortable: true,
+                width: "80px"
+            },
+            {
+                title: "FechaInicio",
+                field: "fechainicio",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: "FechaFin",
+                field: "fechafin",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: "DiasSolicitados",
+                field: "diassolicitados",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: "Estatus",
+                field: "estatus",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: "UsuarioAutorizador",
+                field: "usuarioautorizador",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: "FechaSolicitud",
+                field: "fechasolicitud",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: "FechaRespuesta",
+                field: "fecharespuesta",
+                align: "center",
+                valign: "middle",
+                sortable: true
+            },
+            {
+                title: colAccionesHeader,
+                field: "operate",
+                align: 'center',
+                width: "100px",
+                clickToSelect: false,
+                events: window.operateEvents,
+                formatter: operateFormatter
+            }
+        ]
+    })
+    table.on('check.bs.table uncheck.bs.table ' +
+        'check-all.bs.table uncheck-all.bs.table',
+        function () {
+            buttonRemove.prop('disabled', !table.bootstrapTable('getSelections').length)
+            buttonExportAll.prop('disabled', !table.bootstrapTable('getSelections').length)
+
+            // save your data, here just save the current page
+            selections = getIdSelections()
+            // push or splice the selections if you want to save all data selections
+        })
+    table.on('all.bs.table', function (e, name, args) {
+        console.log(name, args)
+    })
+    buttonRemove.click(function () {
+        askConfirmation(dlgDeleteTitle, dlgDeleteQuestion, function () {
+            let oParams = { ids: selections };
+
+            doAjax(
+                "/ERP/Vacaciones/DeleteVacaciones",
+                oParams,
+                function (resp) {
+                    if (resp.tieneError) {
+                        showError(dlgDeleteTitle, resp.mensaje);
+                        return;
+                    }
+
+                    table.bootstrapTable('remove', {
+                        field: 'id',
+                        values: selections
+                    })
+                    selections = [];
+                    buttonRemove.prop('disabled', true);
+                    buttonExportAll.prop('disabled', true);
+
+                    let e = document.querySelector("[name='refresh']");
+                    e.click();
+
+                    showSuccess(dlgDeleteTitle, resp.mensaje);
+                }, function (error) {
+                    showError(dlgDeleteTitle, error);
+                },
+                postOptions
+            );
+
+        });
+    })
+}
