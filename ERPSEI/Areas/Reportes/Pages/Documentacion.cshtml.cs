fa2c616ea88e02ea9c6e376b61ae5075585a5341
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2025-12-01 15:05:07 -0600

    Creación de archivos Documentación (Políticas) con archivo de recursos

---
+++ ERPSEI/Areas/Reportes/Pages/Documentacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ERPSEI.Areas.Reportes.Pages
+{
+    public class DocumentacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
