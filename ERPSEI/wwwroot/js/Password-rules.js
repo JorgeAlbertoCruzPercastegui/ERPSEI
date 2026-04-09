0239016897cc4fc6b93744d7fcc2b69796a74bc7
JorgeAlbertoCruzPercastegui <jacp@soportecliente.com.mx>
2026-04-09 14:17:24 -0600

    Modificación del diseño de reglas del login y diseño de layout

--- ERPSEI/wwwroot/js/Password-rules.js
+++ ERPSEI/wwwroot/js/Password-rules.js
@@ -1,7 +1,5 @@
-﻿// wwwroot/js/password-rules.js
-(function () {
+﻿(function () {
 
-    // Asegura que exista jQuery
     if (typeof window.jQuery === "undefined") {
         console.error("password-rules.js requiere jQuery.");
         return;
@@ -12,40 +10,54 @@
         el.toggleClass("ok", !!ok);
     }
 
-    function evalPasswordRules(pwd) {
+    function getPasswordValidation(pwd) {
         pwd = pwd || "";
 
-        // Si está vacío, NO marques nada como OK
-        if (pwd.length === 0) {
-            setRule("rule-digit", false);
-            setRule("rule-special", false);
-            setRule("rule-upper", false);
-            setRule("rule-no-repeat", false);
-            setRule("rule-length", false);
-            return;
-        }
-
         const hasDigit = /[0-9]/.test(pwd);
-        const hasSpecial = new RegExp("[!@#$%^&*]").test(pwd);
+        const hasSpecial = /[!@#$%^&*]/.test(pwd);
         const hasUpper = /[A-ZÁÉÍÓÚÑ]/.test(pwd);
         const noRepeatConsecutive = !/(.)\1/.test(pwd);
         const minLen = pwd.length >= 8;
 
-        setRule("rule-digit", hasDigit);
-        setRule("rule-special", hasSpecial);
-        setRule("rule-upper", hasUpper);
-        setRule("rule-no-repeat", noRepeatConsecutive);
-        setRule("rule-length", minLen);
+        return {
+            hasDigit,
+            hasSpecial,
+            hasUpper,
+            noRepeatConsecutive,
+            minLen,
+            isValid: hasDigit && hasSpecial && hasUpper && noRepeatConsecutive && minLen
+        };
+    }
+
+    function evalPasswordRules(pwd) {
+        const result = getPasswordValidation(pwd);
+
+        setRule("rule-digit", result.hasDigit);
+        setRule("rule-special", result.hasSpecial);
+        setRule("rule-upper", result.hasUpper);
+        setRule("rule-no-repeat", result.noRepeatConsecutive);
+        setRule("rule-length", result.minLen);
+
+        return result.isValid;
+    }
+
+    function showRules() {
+        $("#pwdRules").removeClass("d-none").addClass("pwd-rules-visible");
+    }
+
+    function hideRules() {
+        $("#pwdRules").addClass("d-none").removeClass("pwd-rules-visible");
     }
 
     function initPasswordUI() {
+        const $form = $("#account");
         const $pwd = $("#userPassword");
         const $btn = $("#btnTogglePwd");
 
-        // Si no existe el input en la página, no hagas nada
         if ($pwd.length === 0) return;
 
-        // Toggle ver/ocultar contraseña (si existe el botón)
+        hideRules();
+
         if ($btn.length > 0) {
             $btn.off("click.passwordRules").on("click.passwordRules", function () {
                 const input = $pwd[0];
@@ -63,17 +75,35 @@
             });
         }
 
-        // Checklist en vivo
-        $pwd.off("input.passwordRules focus.passwordRules")
-            .on("input.passwordRules focus.passwordRules", function () {
-                evalPasswordRules($(this).val());
-            });
+        $pwd.off("input.passwordRules").on("input.passwordRules", function () {
+            const isValid = evalPasswordRules($(this).val());
 
-        // Inicial
-        evalPasswordRules($pwd.val());
+            if ($("#pwdRules").hasClass("pwd-rules-visible")) {
+                if (isValid) {
+                    hideRules();
+                } else {
+                    showRules();
+                }
+            }
+        });
+
+        $form.off("submit.passwordRules").on("submit.passwordRules", function (e) {
+            const pwd = $pwd.val() || "";
+            const isValid = evalPasswordRules(pwd);
+
+            if (!isValid) {
+                e.preventDefault();
+                showRules();
+                $pwd.trigger("focus");
+                return false;
+            }
+
+            if (typeof showLoading === "function") {
+                showLoading();
+            }
+        });
     }
 
-    // Inicializa cuando el DOM esté listo
     $(document).ready(initPasswordUI);
 
 })();
